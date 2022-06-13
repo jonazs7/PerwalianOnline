3d041238db1c9a46b8019cdd5c6eee20f4c58ead
jonazs7 <yonathan.sebastian@si.ukdw.ac.id>
2022-06-14 04:04:56 +0700

    make it better

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,17 +7,17 @@
 </head>
 
 <!-- Tell the browser to be responsive to screen width -->
-<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="{{asset('public')}}/lte/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="{{asset('lte')}}/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{asset('public')}}/lte/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="{{asset('lte')}}/bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="{{asset('public')}}/lte/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="{{asset('lte')}}/bower_components/Ionicons/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('public')}}/lte/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="{{asset('lte')}}/dist/css/AdminLTE.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="{{asset('public')}}/lte/plugins/iCheck/square/blue.css">
+  <link rel="stylesheet" href="{{asset('lte')}}/plugins/iCheck/square/blue.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -28,6 +28,7 @@
 
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
 
 <body class="hold-transition login-page">
 <div class="login-box">
@@ -72,11 +73,11 @@
 <!-- /.login-box -->
 
 <!-- jQuery 3 -->
-<script src="{{asset('public')}}/lte/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="{{asset('lte')}}/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="{{asset('public')}}/lte/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="{{asset('lte')}}/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- iCheck -->
-<script src="{{asset('public')}}/lte/plugins/iCheck/icheck.min.js"></script>
+<script src="{{asset('lte')}}/plugins/iCheck/icheck.min.js"></script>
 <script>
   $(function () {
     $('input').iCheck({
